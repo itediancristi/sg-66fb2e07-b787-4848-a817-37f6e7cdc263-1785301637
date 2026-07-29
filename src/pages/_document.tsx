ad75086003ecef7132d2817713e2e0b94fffa4f0
softgenai <admin@softgen.ai>
2026-07-29 10:09:26 +0000

    feat(ui): update favicon for logo integration

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/favicon.png" />
         <SEOElements />
         {/*
           CRITICAL: DO NOT REMOVE THIS SCRIPT
